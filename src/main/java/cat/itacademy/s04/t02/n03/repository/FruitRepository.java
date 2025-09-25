bf175d2344b9882043e08e077ddc10b7606adee4
JavierDolo <doralozier@gmail.com>
2025-09-25 15:09:41 +0200

    upload model, exceptions and repository and .env

--- src/main/java/cat/itacademy/s04/t02/n03/repository/FruitRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n03/repository/FruitRepository.java
@@ -1,4 +1,11 @@
 package cat.itacademy.s04.t02.n03.repository;
 
-public class FruitRepository {
+import cat.itacademy.s04.t02.n03.model.Fruit;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface FruitRepository extends MongoRepository<Fruit, String> {
+    Optional<Fruit> findByName(String name);
+    boolean existsByName(String name);
 }
